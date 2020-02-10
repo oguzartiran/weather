8b94093e604f0c82b198e4baa7b515c8bd595d79
oguzartiran <artiranoguz@gmail.com>
2020-02-10 06:30:15 +0300

    api tokens updated.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/5e50f72e40fecc29bd67d95db3c28ac9/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoib2d1emFydGlyYW4iLCJhIjoiY2s2ZDY4MGJjMDEzcjNycXBxcHA2eXU2cyJ9._BscNF-Mb-uYr7YC9BW_pA'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
